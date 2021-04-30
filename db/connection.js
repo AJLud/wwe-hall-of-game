d83c423bc7a5e73e52e01d9765058b57f17b71f6
AJLud <a.ludlowww@gmail.com>
2021-04-30 12:29:27 +0100

    changed position of require() - dotenv in connection

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,10 @@
 // make a connection to your database here
 // and export it so that you can use it to interact with your database
-require("dotenv").config({ path: path.resolve(__dirname, `../.env.${ENV}`) });
+
 const { Pool } = require("pg");
 const path = require("path");
+
+require("dotenv").config({ path: path.resolve(__dirname, `../.env.${ENV}`) });
 
 const ENV = process.env.NODE_ENV || "development";
 
